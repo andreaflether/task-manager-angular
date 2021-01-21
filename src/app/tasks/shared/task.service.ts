76e5d28cbd9852ca6270c571ab3947a3bb0f5911
Andréa Alencar <andreaflether@gmail.com>
2021-01-21 12:50:26 -0300

    Removing TASK constant and adding .data

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -6,14 +6,6 @@ import 'rxjs/add/operator/map';
 
 import { Task } from './task.model';
 
-const TASKS: Task[] = [
-  { id: 1, title: 'Reassistir POI' },
-  { id: 2, title: 'Passar pra liga Prata III' },
-  { id: 3, title: 'Parar de ser trouxa' },
-  { id: 4, title: 'Parar de ser c***** de m*******' },
-  { id: 5, title: 'Ignorar a existência de certas pessoas' }
-];
-
 @Injectable()
 
 export class TaskService {
@@ -23,7 +15,7 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get(this.tasksUrl)
-      .map((response: Response) => response.json() as Task[])
+      .map((response: Response) => response.json().data as Task[])
   }
 
   getImportantTasks(): Observable<Task[]> {
