1f8cfc9f16923707c293ad379ab2a19854588345
Andréa Alencar <andreaflether@gmail.com>
2021-01-27 00:05:33 -0300

    Adding SignUpFormComponent base template and a few adjustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 const ROUTES = RouterModule.forRoot([
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'sign-up', component: SignUpFormComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'tasks/:id', component: TaskDetailComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
