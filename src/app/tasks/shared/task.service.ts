290358795755a4bc079d30d791d557553b7d13c9
Andréa Alencar <andreaflether@gmail.com>
2021-02-01 12:26:23 -0300

    Updating TaskService according to Angular2Token

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -1,54 +1,57 @@
-import { Headers, Http, Response } from '@angular/http';
+import { Headers, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
+
+import { Angular2TokenService } from 'angular2-token';
 
 import { Task } from './task.model';
 
 @Injectable()
 
 export class TaskService {
-  public tasksUrl = 'http://api.task-manager.test:3000/tasks';
-  public headers = new Headers({
-    'Content-Type': 'application/json'
-  });
+  public tasksUrl = 'tasks';
 
-  constructor(private http: Http) { } 
+  constructor(private tokenHttp: Angular2TokenService) { } 
 
   getAll(): Observable<Task[]> {
-    return this.http.get(this.tasksUrl)
+    let url = `${this.tasksUrl}?q[s]=updated_at+DESC`;
+
+    return this.tokenHttp.get(url)
       .catch(this.handleErrors)
-      .map((response: Response) => response.json().data as Task[])
+      .map((response: Response) => this.responseToTasks(response))
   }
 
   getImportant(): Observable<Task[]> {
-    return this.getAll()
+    let url = `${this.tasksUrl}?q[s]=deadline+ASC`
+
+    return this.tokenHttp.get(url)
       .catch(this.handleErrors)
-      .map(tasks => tasks.slice(0, 3));
+      .map((response: Response) => this.responseToTasks(response));
   }
   
   getById(id: number): Observable<Task> {
     let url = `${this.tasksUrl}/${id}`;
 
-    return this.http.get(url)
+    return this.tokenHttp.get(url)
       .catch(this.handleErrors)
-      .map((response: Response) => response.json().data as Task)  
+      .map((response: Response) => this.responsetoTask(response));  
   }
 
   create(task: Task): Observable<Task> {
     let url = this.tasksUrl;
     let body = JSON.stringify(task);
 
-    return this.http.post(url, body, { headers: this.headers })
+    return this.tokenHttp.post(url, body)
       .catch(this.handleErrors)  
-      .map(response => response.json().data as Task)
+      .map((response: Response) => this.responsetoTask(response));
   }
 
   update(task: Task): Observable<Task> {
     let url = `${this.tasksUrl}/${task.id}`;
     let body = JSON.stringify(task)
 
-    return this.http.put(url, body, { headers: this.headers })
+    return this.tokenHttp.put(url, body)
       .catch(this.handleErrors)
       .map(() => task)
   }
@@ -56,21 +59,47 @@ export class TaskService {
   delete(id: Number): Observable<null> {
     let url = `${this.tasksUrl}/${id}`;
 
-    return this.http.delete(url, { headers: this.headers })
+    return this.tokenHttp.delete(url)
       .catch(this.handleErrors)
       .map(() => null)
   }
 
   searchByTitle(term: string): Observable<Task[]> {
-    let url = `${this.tasksUrl}?title=${term}`;
+    let url = `${this.tasksUrl}?q[title_cont]=${term}`;
 
-    return this.http.get(url)
+    return this.tokenHttp.get(url)
       .catch(this.handleErrors)
-      .map((response: Response) => response.json().data as Task[])
+      .map((response: Response) => this.responseToTasks(response));
   }
-
   private handleErrors(error: Response) {
     console.log("Saving error in log file - Error details => ", error)
     return Observable.throw(error);
+  }
+
+  private responseToTasks(response: Response): Task[] {
+    let collection = response.json().data as Array<any>;
+    let tasks: Task[] = [];
+
+    collection.forEach(item => {
+      let task = new Task(
+        item.id,
+        item.attributes.title,
+        item.attributes.description,
+        item.attributes.done,
+        item.attributes.deadline,
+      )
+      tasks.push(task);
+    })
+    return tasks;
+  }
+
+  private responsetoTask(response: Response): Task {
+    return new Task(
+      response.json().data.id,
+      response.json().data.attributes.title,
+      response.json().data.attributes.description,
+      response.json().data.attributes.done,
+      response.json().data.attributes.deadline,
+    )
   }
 }
